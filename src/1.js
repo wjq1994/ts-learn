80c7059f5f0df4dc5d01c781a4b4a588b68da66b
wjq1994 <1194497392@qq.com>
2020-02-05 10:57:54 +0800

    新增class

--- src/1.js
+++ src/1.js
@@ -1,1 +1,67 @@
-var name = 10;
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { 
+                for (var p in b) {
+                    console.log(p)
+                    if (b.hasOwnProperty(p)) 
+                    d[p] = b[p]; 
+                }
+            };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Person = /** @class */ (function () {
+    function Person(name) {
+        this.name = '666';
+        this.name = name;
+    }
+    Object.defineProperty(Person.prototype, "getName", {
+        get: function () {
+            return this.name;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Person.prototype, "setName", {
+        set: function (name) {
+            this.name = name;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Person;
+}());
+var Student = /** @class */ (function (_super) {
+    __extends(Student, _super);
+    function Student(name, stuNo) {
+        var _this = _super.call(this, name) || this;
+        _this._stuNo = "";
+        _this._stuNo = stuNo;
+        return _this;
+    }
+    Object.defineProperty(Student.prototype, "getstuNo", {
+        get: function () {
+            return this._stuNo;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Student.prototype, "setstuNo", {
+        set: function (value) {
+            this._stuNo = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Student;
+}(Person));
+var student1 = new Student("zhangsan", "1203020118");
+student1.setName = "dgdgdfg";
+console.log(student1.getName);
--- src/1.ts
+++ src/1.ts
@@ -1,3 +1,35 @@
-let name: number = 10;
+class Person {
+    name: string = '666';
+    constructor(name: string) {
+        this.name = name;
+    }
+    get getName() {
+        return this.name;
+    }
+    set setName(name: string) {
+        this.name = name;
+    }
+}
 
-export { }
+class Student extends Person {
+    private _stuNo: string = "";
+    public get getstuNo(): string {
+        return this._stuNo;
+    }
+    public set setstuNo(value: string) {
+        this._stuNo = value;
+    }
+    constructor(name: string, stuNo: string) {
+        super(name);
+        this._stuNo = stuNo;
+    }
+
+}
+
+let student1 = new Student("zhangsan", "1203020118");
+
+student1.setName = "dgdgdfg";
+
+console.log(student1.getName);
+
+export { };
